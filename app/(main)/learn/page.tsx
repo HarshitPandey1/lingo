3a6fea376fae8291545c3d072a190f7bf8067cf1
Harshit <pharshit2003@gmail.com>
2024-03-23 10:05:45 +0530

    third commit

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -4,16 +4,13 @@ import { Header } from "./header";
 
 const LearnPage=()=>{
     return(
-        // <div className="flex flex-row-reverse gap-[48px] px-6">
-        //     <StickyWrapper>
-        //         My sticky sidebar
-        //     </StickyWrapper>
-        //     <FeedWrapper>
-        //         <Header title="Spanish"></Header>
-        //     </FeedWrapper>
-        // </div>
-        <div>
-            Learn Page
+        <div className="flex flex-row-reverse gap-[48px] px-6">
+            <StickyWrapper>
+                My sticky sidebar
+            </StickyWrapper>
+            <FeedWrapper>
+                <Header title="Spanish"></Header>
+            </FeedWrapper>
         </div>
     );
 };
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -16,7 +16,7 @@ type Props = {
 
 export const Sidebar = ({ className }: Props) => {
     return (
-        <div className={cn("flex h-full lg:w-[255px] lg:fixed left=0 top-0 px-4 border-r-2 flex-col", className
+        <div className={cn("flex h-full lg:w-[255px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col", className
         )}>
             <Link href="/learn">
                 <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
