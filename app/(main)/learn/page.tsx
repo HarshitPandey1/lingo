ecb566e9ba97b33660ea8970afaf2be3aa91b2e2
Harshit <pharshit2003@gmail.com>
2024-03-25 11:55:57 +0530

    loader in learn page

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -2,8 +2,19 @@ import { StickyWrapper } from "@/components/sticky-wrapper";
 import { FeedWrapper } from "@/components/feed-wrapper";
 import { Header } from "./header";
 import { UserProgress } from "@/components/user-progress";
+import { getUserProgress } from "@/db/queries";
+import { userProgress } from "@/db/schema";
+import { redirect } from "next/navigation";
 
-const LearnPage=()=>{
+const LearnPage= async()=>{
+    const userProgressData=getUserProgress();
+
+    const [
+        userProgress
+    ]=await Promise.all([userProgressData]);
+    if(!userProgress || !userProgress.activeCourses){
+        redirect ("/courses");
+    }
     
     return(
         <div className="flex flex-row-reverse gap-[48px] px-6">
